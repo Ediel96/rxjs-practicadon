d09439f75d98da886c888de5d0c90ff140988bad
Ediel96 <h.amiltone.diel@gmail.com>
2020-06-09 00:17:08 -0500

    map

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { range, fromEvent } from 'rxjs';
+import { map, pluck, mapTo } from 'rxjs/operators';
+
+// range(1,5).pipe(
+//     map<number,string>( val => (val * 10).toString() )
+// )
+// .subscribe( console.log );
+
+const keyup$ = fromEvent<KeyboardEvent>( document, 'keyup' );
+
+const keyupCode$ = keyup$.pipe(
+    map( event => event.code )
+);
+
+
+keyupCode$.subscribe(res => console.log('map' , res))
+
